05c17e4b381ebf5507ff9bb601578671c48f0dec
Samuel Jeon <samueljeon@Samuels-Air.attlocal.net>
2017-07-23 00:29:12 -0500

    files

---
+++ app/data/friends.js
@@ -0,0 +1,109 @@
+var friendsArray = [{
+	name: "Sam",
+	photo: "",
+	scores:[
+		2,
+		1,
+		5,
+		3,
+		1,
+		5,
+		3,
+		2,
+		2,
+		5
+	]
+},{
+	name: "Joey",
+	photo: "",
+	scores:[
+		4,
+		1,
+		3,
+		5,
+		1,
+		2,
+		3,
+		4,
+		2,
+		5
+	]
+},{
+	name: "Chandler",
+	photo: "",
+	scores:[
+		3,
+		1,
+		2,
+		3,
+		5,
+		5,
+		1,
+		2,
+		1,
+		2
+	]
+},{
+	name: "Ross",
+	photo: "",
+	scores:[
+		2,
+		2,
+		5,
+		4,
+		1,
+		3,
+		3,
+		1,
+		2,
+		3
+	]
+},{
+	name: "Monica",
+	photo: "",
+	scores:[
+		4,
+		3,
+		5,
+		2,
+		1,
+		1,
+		2,
+		2,
+		3,
+		5
+	]
+},{
+	name: "Rachel",
+	photo: "",
+	scores:[
+		4,
+		1,
+		2,
+		3,
+		5,
+		4,
+		2,
+		1,
+		1,
+		1
+	]
+},{
+	name: "Phoebe",
+	photo: "",
+	scores:[
+		1,
+		1,
+		1,
+		1,
+		2,
+		2,
+		3,
+		4,
+		4,
+		5
+	]
+}
+];
+
+module.exports = friendsArray;
